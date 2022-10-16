ae8ea97ac99d8a2593512b044c6130f9f4f5d40e
tomasnapolitano <t97napolitano@gmail.com>
2022-10-16 17:06:29 -0300

    fixed temporary bootstrap config

--- Config/Config.php
+++ Config/Config.php
@@ -2,11 +2,11 @@
 
 define("ROOT", dirname(__DIR__) . "/");
 //Path to your project's root folder
-define("FRONT_ROOT", "/PetHero/");
+define("FRONT_ROOT", "/ExerciseOne/");
 define("VIEWS_PATH", "Views/");
 define("CSS_PATH", FRONT_ROOT.VIEWS_PATH . "layout/styles/");
 define("JS_PATH", FRONT_ROOT.VIEWS_PATH . "js/");
-define("IMG_PATH", FRONT_ROOT.VIEWS_PATH . "img/");
+define("IMG_PATH", VIEWS_PATH . "img/");
 ?>
 
 
