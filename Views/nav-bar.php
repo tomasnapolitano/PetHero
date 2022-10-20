2f343b7424456d3ab6b5cb10bbde61f3733a6087
tomasnapolitano <t97napolitano@gmail.com>
2022-10-20 19:05:05 -0300

    added link to home in PetHero title

--- Views/nav-bar.php
+++ Views/nav-bar.php
@@ -1,7 +1,7 @@
 <div class="wrapper row1">
   <header id="header" class="clear"> 
     <div id="logo" class="fl_left">
-      <h1>PetHero</h1>
+      <a href="<?php echo FRONT_ROOT . "Home/ShowHomeView" ?>"><h1>PetHero</h1></a>
     </div>
     <nav id="mainav" class="fl_right">
       <ul class="clear">
