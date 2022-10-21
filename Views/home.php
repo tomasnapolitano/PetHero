3faf45e48294448c97a9225c5cb3c62d31aa5a52
DiegoArzondo <n17_94@hotmail.com>
2022-10-20 23:56:14 -0300

    Update home.php
    link a keeper list

--- Views/home.php
+++ Views/home.php
@@ -39,7 +39,7 @@
 </div>
   <div class="div-login">  
     <a href="<?php echo FRONT_ROOT . "Dog/ShowAddView"?>">Add Pet</a>
-    <a href="<?php echo FRONT_ROOT . ""?>">Search Keepers</a>
+    <a href="<?php echo FRONT_ROOT . "Owner/ShowKeeperListView"?>">Search Keepers</a>
     <a href="<?php echo FRONT_ROOT . "Dog/ShowPetList"?>">View My Pets</a>
     <?php if ($_SESSION['loggedUser']->getUserRole()!=2) { ?>
     <a href="<?php echo FRONT_ROOT . "Keeper/ShowRegisterView"?>">Be a Keeper!</a>
