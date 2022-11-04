bf9ccfddd71200adf91a91efd3096d88b616db2c
tomasnapolitano <t97napolitano@gmail.com>
2022-11-04 14:20:45 -0300

    added bootstrap datepicker

--- Views/keeper-list.php
+++ Views/keeper-list.php
@@ -6,6 +6,7 @@ use Models\Keeper;
 use Models\Owner;
 
 ?>
+
 <!-- ################################################################################################ -->
 <div class="wrapper row2 bgded" style="background-image:url('../images/demo/backgrounds/1.png');">
   <div class="overlay">
@@ -31,9 +32,48 @@ use Models\Owner;
 <div class="wrapper row4">
   <main class="hoc container clear"> 
     <!-- main body -->
+    
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
+
     <div class="content"> 
       <div class="scrollable">
       <form action="<?php echo FRONT_ROOT . "Owner/ShowKeeperListView" ?>" method="get">
+
+
+     <section class="container">
+        <h3 class="pt-4 pb-2">Search Keepers!</h3>
+        <form>
+            <div class="row form-group">
+                <label for="date" class="col-sm-1 col-form-label">Date</label>
+                <div class="col-sm-4">
+                    <div class="input-group date" id="datepicker">
+                        <input type="text" class="form-control">
+                        <span class="input-group-append">
+                            <span class="input-group-text bg-white">
+                                <i class="fa fa-calendar"></i>
+                            </span>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </section>
+
+    <script type="text/javascript">
+        $(function() {
+            $('#datepicker').datepicker({
+                format: 'dd-M-yyyy',
+                lan:'en',
+                multidate: true
+            });
+        });
+    </script>
+
         <table style="text-align:center;">
           <thead>
             <tr>
